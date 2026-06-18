96666b771cfe1e7de753d0b3aebea829f3d1e9fd
kiinami <kinami@posteo.net>
2026-06-18 09:19:16 +0200

    chore: small changes

--- src/lisa/utils/common.h
+++ src/lisa/utils/common.h
@@ -9,36 +9,44 @@
 #include <vulkan/vulkan_raii.hpp>
 
 namespace lisa {
-  typedef int8_t int8;
-  typedef int32_t int32;
-  typedef int64_t int64;
-  typedef uint8_t uint8;
-  typedef uint16_t uint16;
-  typedef uint32_t uint32;
-  typedef uint64_t uint64;
-  typedef size_t size;
+  // Numbers
+  using int8 = int8_t;
+  using int32 = int32_t;
+  using int64 = int64_t;
+  using uint8 = uint8_t;
+  using uint16 = uint16_t;
+  using uint32 = uint32_t;
+  using uint64 = uint64_t;
+  using size = size_t;
 
-  typedef std::string str;
+  // Strings
+  using str = std::string;
+  using path = std::filesystem::path;
+
+  // Collections
   template<typename T> using vector = std::vector<T>;
   template<typename K, typename V, typename H = std::hash<K>>
   using umap = std::unordered_map<K, V, H>;
   template<typename T, typename U> using pair = std::pair<T, U>;
   template<typename T, size S> using array = std::array<T, S>;
-  typedef std::filesystem::path path;
-  template<typename T> using uptr = std::unique_ptr<T>;
   template<typename T> using uset = std::unordered_set<T>;
+
+  // Pointers
+  template<typename T> using uptr = std::unique_ptr<T>;
+  template<typename T> using sptr = std::shared_ptr<T>;
+  template<typename T> using wptr = std::weak_ptr<T>;
+
+  // Optionals
   template<typename T> using optional = std::optional<T>;
-
-  typedef glm::vec2 vec2;
-  typedef glm::vec3 vec3;
-  typedef glm::vec4 vec4;
-  typedef glm::vec3 rgb;
-  typedef glm::vec4 rgba;
-
-  typedef glm::quat quat;
-
-  typedef glm::mat3 mat3;
-  typedef glm::mat4 mat4;
-
   using std::nullopt;
+
+  // Maths
+  using vec2 = glm::vec2;
+  using vec3 = glm::vec3;
+  using vec4 = glm::vec4;
+  using rgb = glm::vec3;
+  using rgba = glm::vec4;
+  using quat = glm::quat;
+  using mat3 = glm::mat3;
+  using mat4 = glm::mat4;
 }
